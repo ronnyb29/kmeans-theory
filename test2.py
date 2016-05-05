78fe688ecb4c5455b5209dc41bb72cdd49363444
ronnyb29 <ronboger@gmail.com>
2016-05-04 23:33:42 -0400

    non promising numerical results

--- test2.py
+++ test2.py
@@ -4,9 +4,9 @@ from sklearn import mixture
 
 np.random.seed(1)
 
-mleArr = np.empty([2, 9])
-bic = np.empty([2, 9])
-mleDifferences = np.empty([2, 8])
+mleArr = np.empty([9, 9])
+bic = np.empty([9, 9])
+mleDifferences = np.empty([9, 8])
 
 
 def verifyConcavity(arr):
@@ -21,7 +21,7 @@ def mle(GMM, X):
 
 numClusters = [i for i in range(1,10)]
 obs = np.random.randn(100, 1)
-for i in [1, 2]:
+for i in xrange(1,10):
 	obs = np.concatenate((obs, i*100 + np.random.randn(100, 1)))
 	for x in xrange(1,10):
 		g = mixture.GMM(n_components=x)
@@ -32,29 +32,25 @@ for i in [1, 2]:
 	mleDifferences[i-1] = np.diff(mleArr[i-1])
 
 
-# 
-# print mleDifferences
+#bic[0] corresponds to 2 'real' cluster centers
 
 #plotting a bunch of stuff
 plt.figure(1)
-plt.plot(numClusters, bic[0])
+plt.plot(numClusters, bic[1])
 plt.ylabel('BIC')
 plt.xlabel('Number of Clusters')
 plt.title('BIC plot')
 plt.show()
 
 plt.figure(2)
-plt.plot(numClusters, mleArr[0])
+plt.plot(numClusters, mleArr[1])
 plt.ylabel('Maximum Likelihood Estimator')
 plt.xlabel('Number of Clusters')
 plt.title('MLE plot')
 plt.show()
 
-print 'shape of numClusters is ' +  np.shape(numClusters[0:-2])
-print 'shape of mleDifferences[0] is ' + np.shape(mleDifferences[0])
-
 plt.figure(3)
-plt.plot(numClusters[0:-2], mleDifferences[0], 'o')
+plt.plot(numClusters[0:-1], mleDifferences[1], 'o')
 plt.title('MLE Differences')
 plt.ylabel('Difference in Maximum Likelihood Estimator')
 plt.xlabel('Number of Clusters')
