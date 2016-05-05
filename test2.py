8fe58f4c7fa42b9a30b21032302bbd7a6d3ca3ee
ronnyb29 <ronboger@gmail.com>
2016-05-05 00:04:33 -0400

    increasing number of iterations has interesting results, but still not what we think

--- test2.py
+++ test2.py
@@ -16,7 +16,7 @@ def verifyConcavity(arr):
 	return True
 
 def mle(GMM, X):
-	return 2 * GMM.score(X).sum()
+	return GMM.score(X).sum() #score computes the log probability under the model., MLE is just the sum (assumes log likelihood)
 			
 
 numClusters = [i for i in range(1,10)]
@@ -24,10 +24,12 @@ obs = np.random.randn(100, 1)
 for i in xrange(1,10):
 	obs = np.concatenate((obs, i*100 + np.random.randn(100, 1)))
 	for x in xrange(1,10):
-		g = mixture.GMM(n_components=x)
+		g = mixture.GMM(n_components=x, n_init=1, n_iter=100,  random_state=1)
 		# generate random observations with two modes centered on 0 and 100 
+		#n init makes it very slow
 		g.fit(obs)
 		mleArr[i - 1, x - 1] = mle(g, obs)
+
 		bic[i - 1, x - 1] = g.bic(obs)
 	mleDifferences[i-1] = np.diff(mleArr[i-1])
 
@@ -36,21 +38,21 @@ for i in xrange(1,10):
 
 #plotting a bunch of stuff
 plt.figure(1)
-plt.plot(numClusters, bic[1])
+plt.plot(numClusters, bic[6])
 plt.ylabel('BIC')
 plt.xlabel('Number of Clusters')
 plt.title('BIC plot')
 plt.show()
 
 plt.figure(2)
-plt.plot(numClusters, mleArr[1])
+plt.plot(numClusters, mleArr[6])
 plt.ylabel('Maximum Likelihood Estimator')
 plt.xlabel('Number of Clusters')
 plt.title('MLE plot')
 plt.show()
 
 plt.figure(3)
-plt.plot(numClusters[0:-1], mleDifferences[1], 'o')
+plt.plot(numClusters[0:-1], mleDifferences[6], 'o')
 plt.title('MLE Differences')
 plt.ylabel('Difference in Maximum Likelihood Estimator')
 plt.xlabel('Number of Clusters')
